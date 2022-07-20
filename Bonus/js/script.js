9b93748cf91967db54e5d45e985d4cc147a3f57d
Gabridg <gabri99.del@gmail.com>
2022-07-20 15:00:39 +0200

    added folder Bonus

---
+++ Bonus/js/script.js
@@ -0,0 +1,27 @@
+console.log('JS OK');
+/*
+Descrizione:
+Attraverso l'apposita API di Boolean
+https://flynn.boolean.careers/exercises/api/random/mail
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+Bonus
+Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+*/
+const app = new Vue({
+    el: '#root',
+    data: {
+        mails: [],
+        numberOfMail: 10
+    },
+    methods: {
+    },
+    created() {
+        for (let i = 1; i <= this.numberOfMail; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((res) => {
+                console.log(res.data.response);
+                const email = res.data.response;
+                this.mails.push(email);
+            })
+        }
+    }
+})
